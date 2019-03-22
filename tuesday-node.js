02ab840442123487d56eac2d0fe4d44fe194533f
Derrick Craven <dcravenus@gmail.com>
2019-03-22 13:51:19 -0600

    Add poster images

--- tuesday-node.js
+++ tuesday-node.js
@@ -1,43 +1,43 @@
-const request = require('request-promise-native');
-const chalk = require('chalk');
+const request = require("request-promise-native");
+const chalk = require("chalk");
 
-const jsdom = require('jsdom');
-const {JSDOM} = jsdom;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 module.exports = async function tuesday() {
   const config = {
-    "theaters": [
+    theaters: [
       {
-        "id": "ci0005105",
-        "name": "Sandy Movies 9"
+        id: "ci0005105",
+        name: "Sandy Movies 9"
       },
       {
-        "id": "ci0014244",
-        "name": "Thanksgiving Point Megaplex"
+        id: "ci0014244",
+        name: "Thanksgiving Point Megaplex"
       },
       {
-        "id": "ci0010304",
-        "name": "Jordan Commons Megaplex"
+        id: "ci0010304",
+        name: "Jordan Commons Megaplex"
       },
       {
-        "id": "ci0012318",
-        "name": "American Fork Cinemark"
+        id: "ci0012318",
+        name: "American Fork Cinemark"
       },
       {
-        "id": "ci92392901",
-        "name": "AMC Classic Provo"
+        id: "ci92392901",
+        name: "AMC Classic Provo"
       },
       {
-        "id": "ci0010268",
-        "name": "Provo Town Centre Mall Cinemark"
+        id: "ci0010268",
+        name: "Provo Town Centre Mall Cinemark"
       },
       {
-        "id": "ci0014231",
-        "name": "South Jordan Megaplex - The District"
+        id: "ci0014231",
+        name: "South Jordan Megaplex - The District"
       }
     ],
 
-    "minScore": 60
+    minScore: 60
   };
 
   const theaters = config.theaters;
@@ -45,21 +45,22 @@ module.exports = async function tuesday() {
   let movies = [];
   const promises = [];
 
-  theaters.forEach((theater)=>{
-    const promise = getTheaterData(theater.id).then((theaterData)=>{
-      theaterData.forEach((theaterMovie)=>{
-        let movieObj = movies.find((movieObj)=>{
+  theaters.forEach(theater => {
+    const promise = getTheaterData(theater.id).then(theaterData => {
+      theaterData.forEach(theaterMovie => {
+        let movieObj = movies.find(movieObj => {
           return movieObj.title === theaterMovie.title;
         });
 
-        if(movieObj){
-          movieObj.showtimes.push({theater: theater.name, times: theaterMovie.showtimes});
+        if (movieObj) {
+          movieObj.showtimes.push({ theater: theater.name, times: theaterMovie.showtimes });
         } else {
           movies.push({
             title: theaterMovie.title,
             metascore: theaterMovie.metascore,
             mpaaRating: theaterMovie.mpaaRating,
             runtime: theaterMovie.runtime,
+            imgUrl: theaterMovie.imgURL,
             showtimes: [
               {
                 theater: theater.name,
@@ -69,81 +70,80 @@ module.exports = async function tuesday() {
           });
         }
       });
-
     });
     promises.push(promise);
- });
+  });
 
- return Promise.all(promises).then(()=>{
-   movies.sort((a,b)=>{
-    if(a.title < b.title) return -1;
-    if(a.title > b.title) return 1;
-    return 0;
-   });
+  return Promise.all(promises).then(() => {
+    movies.sort((a, b) => {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    });
 
-   movies = movies.filter((movie)=>{
-    return movie.metascore >= config.minScore;
-   });
+    movies = movies.filter(movie => {
+      return movie.metascore >= config.minScore;
+    });
 
-   movies.sort((a, b) => {
-    return (a.metascore - b.metascore) * -1;
-   });
+    movies.sort((a, b) => {
+      return (a.metascore - b.metascore) * -1;
+    });
 
-
-   return movies;
- });
-
-
-}
+    return movies;
+  });
+};
 
 function getTheaterData(theaterId) {
   let showDate = getTuesdayShowDate();
-  return request(`https://www.imdb.com/showtimes/cinema/US/${theaterId}/${showDate}`)
-  .then((response) => {
-    const movies = [];
+  return request(`https://www.imdb.com/showtimes/cinema/US/${theaterId}/${showDate}`).then(
+    response => {
+      const movies = [];
 
-    const dom = new JSDOM(response);
-    const movieDivs = dom.window.document.querySelectorAll('div.list_item');
-    for(let i = 0; i<movieDivs.length; i++) {
-      const div = movieDivs[i];
-      let title = div.querySelector('h3 a').textContent;
-      let showtimeLinks = div.querySelectorAll('div.showtimes > a');
-      const showtimes = [];
-      for(let j =0; j<showtimeLinks.length; j++){
-        showtimes.push(showtimeLinks[j].textContent);
+      const dom = new JSDOM(response);
+      const movieDivs = dom.window.document.querySelectorAll("div.list_item");
+      for (let i = 0; i < movieDivs.length; i++) {
+        const div = movieDivs[i];
+        let title = div.querySelector("h3 a").textContent;
+        let showtimeLinks = div.querySelectorAll("div.showtimes > a");
+        const showtimes = [];
+        for (let j = 0; j < showtimeLinks.length; j++) {
+          showtimes.push(showtimeLinks[j].textContent);
+        }
+
+        const metascoreSpan = div.querySelector("span.metascore");
+        let metascore;
+        if (metascoreSpan) {
+          metascore = parseInt(div.querySelector("span.metascore").textContent);
+        }
+
+        let mpaaRatingTag = div.querySelector("img.certimage");
+        let mpaaRating = "";
+        if (mpaaRatingTag) {
+          mpaaRating = mpaaRatingTag.title;
+        }
+
+        const runtimeTag = div.querySelector("p.cert-runtime-genre time");
+        let runtime = "";
+        if (runtimeTag) {
+          runtime = runtimeTag.textContent;
+        }
+
+        const imgTag = div.querySelector("div.image img");
+        let imgURL = imgTag.src;
+
+        movies.push({ title, showtimes, metascore, mpaaRating, runtime, imgURL });
       }
 
-      const metascoreSpan = div.querySelector('span.metascore');
-      let metascore;
-      if(metascoreSpan) {
-        metascore = parseInt(div.querySelector('span.metascore').textContent);
-      }
-
-      let mpaaRatingTag = div.querySelector('img.certimage');
-      let mpaaRating = '';
-      if(mpaaRatingTag) {
-        mpaaRating = mpaaRatingTag.title;
-      }
-
-      const runtimeTag = div.querySelector('p.cert-runtime-genre time');
-      let runtime = '';
-      if(runtimeTag) {
-        runtime = runtimeTag.textContent;
-      }
-
-      movies.push({title, showtimes, metascore, mpaaRating, runtime});
+      return movies;
     }
-
-    return movies;
-
-  });
+  );
 }
 
 function getTuesdayShowDate() {
   const today = new Date();
   const day = today.getDay();
   let tuesday;
-  switch(day){
+  switch (day) {
     case 0:
       tuesday = today.setDate(today.getDate() + 2);
       break;
@@ -175,7 +175,7 @@ function getTuesdayShowDate() {
   let padDate = "" + today.getDate();
   padDate = pad.substring(0, pad.length - padDate.length) + padDate;
 
-  showDate = today.getUTCFullYear() + '-' + padMonth + '-' + padDate;
+  showDate = today.getUTCFullYear() + "-" + padMonth + "-" + padDate;
 
   return showDate;
 }
